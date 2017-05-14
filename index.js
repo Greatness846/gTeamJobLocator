69a0367cf497ede2a824f2c42361f21c797c9a80
Coleen Rowe <afm08c@gmail.com>
2017-05-14 06:21:26 -0400

    Fixerup

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-const server = app.listen(3000);
-
 
 app.post('/send', (req, res) => {
     // Send SMS
@@ -25,4 +23,7 @@ app.post('/send', (req, res) => {
     "12035338119", "1" + userData.toNumber, userData.message,
     (err, responseData) => {if (err) {console.log(err)}}
   );
+
 });
+
+const server = app.listen(process.env.port || process.env.PORT || 3000, function(){console.log("Project Running"});
